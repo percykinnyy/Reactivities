351a3e9864c89cef89afdff4ccc9f03aa48dbaea
Percy Kinny <percykinny@outlook.com>
2024-02-14 11:31:10 +0530

    end of section #3

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
---
+++ client-app/src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { useEffect } from "react";
+import './App.css';
+import axios from 'axios';
+import { Header, Icon, List, ListItem } from 'semantic-ui-react';
+
+function App() {
+  const [activities,setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+    .then(response => {
+        console.log(response);
+        setActivities(response.data)
+    })
+  },[])
+
+  return (
+    <div>
+         <Header as='h2' icon>
+          <Icon name='settings' />
+            Reactivities
+         </Header> 
+        <List>
+          {activities.map((activity: any) => (
+            <ListItem key = {activity.id}>
+              {activity.title}
+            </ListItem>
+          )) 
+           }
+        </List>
+    </div>
+    
+  )
+}
+
+export default App
